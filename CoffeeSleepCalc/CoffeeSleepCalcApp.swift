146597ebed4b1cc999fe21488a782b9b710d6dce
Basith P <basithpofficial@gmail.com>
2024-10-29 06:36:50 +0530

    Initial Commit

---
+++ CoffeeSleepCalc/CoffeeSleepCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeSleepCalcApp.swift
+//  CoffeeSleepCalc
+//
+//  Created by Basith on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeSleepCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
