ba2d000c137749ce324c52ab74004d3d6425f30f
Basith P <basithpofficial@gmail.com>
2024-10-29 07:23:38 +0530

    get prediction from model and show result to user

--- CoffeeSleepCalc/ContentView.swift
+++ CoffeeSleepCalc/ContentView.swift
@@ -5,12 +5,17 @@
 //  Created by Basith on 29/10/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffees = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMsg = ""
+    @State private var showAlert = false
     
     var body: some View {
         NavigationStack {
@@ -34,11 +39,35 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("CS Calc")
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMsg)
+            }
         }
     }
     
     func calculateSleep() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let timeComponent = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hours = (timeComponent.hour ?? 0) * 60 * 60
+            let minutes = (timeComponent.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hours + minutes), estimatedSleep: sleepAmount, coffee: Double(coffees))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is"
+            alertMsg = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMsg = "Something went wrong while calculating sleep time."
+        }
         
+        showAlert = true
     }
 }
 
