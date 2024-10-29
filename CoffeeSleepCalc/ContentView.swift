4cccb7748b27fa8cecca602361828f0ad007dfe1
Basith P <basithpofficial@gmail.com>
2024-10-29 06:54:31 +0530

    develop UI

--- CoffeeSleepCalc/ContentView.swift
+++ CoffeeSleepCalc/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffees = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?").font(.headline)
+                DatePicker("Wake Up", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                    .padding(.bottom)
+                
+                Text("Desired amount of sleep").font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    .padding(.bottom)
+                
+                Text("Daily coffee intake").font(.headline)
+                Stepper("\(coffees) cup(s)", value: $coffees, in: 0...20)
+                    .padding(.bottom)
+                
+                Button("Calculate", action: calculateSleep)
+                    .buttonStyle(.borderedProminent)
+                    .controlSize(.large)
+            }
+            .padding()
+            .navigationTitle("CS Calc")
         }
-        .padding()
+    }
+    
+    func calculateSleep() {
+        
     }
 }
 
